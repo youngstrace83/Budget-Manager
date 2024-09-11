048a0653e892b25cf2273faf4da708e50dfe61ae
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-11 09:39:44 -0400

    Added a spend function, that also returns remaining amount, and a print summary.

--- budget.py
+++ budget.py
@@ -4,7 +4,26 @@ expenditure = {}
 
 def add_budget(name, amount):
     global available
+    if name in budgets:
+        raise ValueError("Budget exists")
+    if amount > available:
+        raise ValueError("Insufficient funds")
     budgets[name] = amount
     available -= amount
     expenditure[name] = 0
     return available
+
+def spend(name, amount):
+    if name not in expenditure:
+        raise ValueError("No such budget")
+    expenditure[name] += amount
+    budgeted = budgets[name]
+    spent = expenditure[name]
+    return budgeted - spent
+
+def print_summary():
+    for name in budgets:
+        budgeted = budgets[name]
+        spent = expenditure[name]
+        remaining = budgeted - spent
+        print(name, budgeted, spent, remaining)
