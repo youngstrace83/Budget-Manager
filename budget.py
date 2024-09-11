162e08d034f24bb5e2f6856fed843949e45c70dc
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-11 10:11:54 -0400

    Expanded on the print summary to show bugeted, spent, and remaining ammounts. Also adjusted print function to be more readable.

--- budget.py
+++ budget.py
@@ -22,8 +22,18 @@ def spend(name, amount):
     return budgeted - spent
 
 def print_summary():
+    print("Budget             Bugeted      Spent  Remaining")
+    print("--------------- ---------- ---------- ----------")
+    total_budgeted = 0
+    total_spent = 0
+    total_remaining = 0
     for name in budgets:
         budgeted = budgets[name]
         spent = expenditure[name]
         remaining = budgeted - spent
-        print(name, budgeted, spent, remaining)
+        print(f'{name:15s} {budgeted:10.2f} {spent:10.2f}' f'{remaining:10.2f}')
+        total_budgeted += budgeted
+        total_spent += spent
+        total_remaining += remaining
+    print("--------------- ---------- ---------- ----------")
+    print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f}' f'{total_budgeted - total_spent:10.2f}')
