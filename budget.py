2a773e24a5a5e135ca22236e538865f19245c5f7
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-11 11:38:29 -0400

    Coverted code into a class, added an initializer, created instance variables, and turned the functions into methods.

--- budget.py
+++ budget.py
@@ -1,39 +1,40 @@
-available = 2500.00
-budgets = {}
-expenditure = {}
+class BudgetManager:
+    def __init__(self, ammount):
+        self.available = ammount
+        self.budgets = {}
+        self.expenditure = {}
 
-def add_budget(name, amount):
-    global available
-    if name in budgets:
-        raise ValueError("Budget exists")
-    if amount > available:
-        raise ValueError("Insufficient funds")
-    budgets[name] = amount
-    available -= amount
-    expenditure[name] = 0
-    return available
+    def add_budget(self, name, amount):
+        if name in self.budgets:
+            raise ValueError("Budget exists")
+        if amount > self.available:
+            raise ValueError("Insufficient funds")
+        self.budgets[name] = amount
+        self.available -= amount
+        self.expenditure[name] = 0
+        return self.available
 
-def spend(name, amount):
-    if name not in expenditure:
-        raise ValueError("No such budget")
-    expenditure[name] += amount
-    budgeted = budgets[name]
-    spent = expenditure[name]
-    return budgeted - spent
+    def spend(self, name, amount):
+        if name not in self.expenditure:
+            raise ValueError("No such budget")
+        self.expenditure[name] += amount
+        budgeted = self.budgets[name]
+        spent = self.expenditure[name]
+        return budgeted - spent
 
-def print_summary():
-    print("Budget             Bugeted      Spent  Remaining")
-    print("--------------- ---------- ---------- ----------")
-    total_budgeted = 0
-    total_spent = 0
-    total_remaining = 0
-    for name in budgets:
-        budgeted = budgets[name]
-        spent = expenditure[name]
-        remaining = budgeted - spent
-        print(f'{name:15s} {budgeted:10.2f} {spent:10.2f}' f'{remaining:10.2f}')
-        total_budgeted += budgeted
-        total_spent += spent
-        total_remaining += remaining
-    print("--------------- ---------- ---------- ----------")
-    print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f}' f'{total_budgeted - total_spent:10.2f}')
+    def print_summary(self):
+        print("Budget             Bugeted      Spent  Remaining")
+        print("--------------- ---------- ---------- ----------")
+        total_budgeted = 0
+        total_spent = 0
+        total_remaining = 0
+        for name in self.budgets:
+            budgeted = self.budgets[name]
+            spent = self.expenditure[name]
+            remaining = budgeted - spent
+            print(f'{name:15s} {budgeted:10.2f} {spent:10.2f}' f'{remaining:10.2f}')
+            total_budgeted += budgeted
+            total_spent += spent
+            total_remaining += remaining
+        print("--------------- ---------- ---------- ----------")
+        print(f'{"Total":15s} {total_budgeted:10.2f} {total_spent:10.2f}' f'{total_budgeted - total_spent:10.2f}')
